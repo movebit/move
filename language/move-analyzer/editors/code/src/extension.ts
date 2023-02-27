e8e111bbd491164b94b14725a7e439813e055753
yuyang <yuyangxxxmail@gmail.com>
2023-02-27 17:00:41 +0800

    some name error

--- language/move-analyzer/editors/code/src/extension.ts
+++ language/move-analyzer/editors/code/src/extension.ts
@@ -8,7 +8,7 @@ import * as vscode from 'vscode';
 /** Information related to this extension itself, such as its identifier and version. */
 export class Extension {
     /** The string used to uniquely identify this particular extension to VS Code. */
-    readonly identifier = 'MoveBit.move-analyzer';
+    readonly identifier = 'MoveBit.sui-move-analyzer';
 
     private readonly extension: vscode.Extension<unknown>;
 
