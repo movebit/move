aa00171d47a4f81b349cb4ff5ac8bbd982ff15a6
yuyang <yuyangxxxmail@gmail.com>
2023-03-13 17:49:54 +0800

    fix a desp

--- language/move-analyzer/src/move_generate_spec_chen.rs
+++ language/move-analyzer/src/move_generate_spec_chen.rs
@@ -1,7 +1,8 @@
 use super::move_generate_spec::FunSpecGenerator;
 use move_compiler::parser::ast::*;
 impl FunSpecGenerator {
-    //  针对加法 减法 移位等运算自动生成 aborts_if
+    // 针对加法 减法 移位等运算可能会参数溢出等异常
+    // 这个函数收集 e种所有的加法减法等操作
     pub(crate) fn collect_add_sub_etc(e: &Exp) -> Vec<&Exp> {
         unimplemented!()
     }
